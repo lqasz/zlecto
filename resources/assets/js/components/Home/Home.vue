bdcda0d52a5a1e1aa0acafb65126d316e9675f83
lqasz <lucas.osuch@wp.pl>
2017-12-03 00:54:22 +0100

    Wyszukiwarka zleceń

--- resources/assets/js/components/Home/Home.vue
+++ resources/assets/js/components/Home/Home.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="container">
-    	<form class="form-inline">
-    		<div class="col-lg-4">
-    			<input type="text" class="form-control" id="szukajZlecen" aria-describedby="wyszukiwarka" placeholder="Jakiej usługi szukasz?">
-    		</div>
-    		<div class="col-lg-4">
-    			<input type="text" class="form-control" id="szukajLokalizacji" aria-describedby="wyszukiwarka" placeholder="Miejscowość lub Województwo">
-    		</div>
-    		<div class="col-lg-4">
-    			<button>Test</button>
+    	<div class="row justify-content-center">
+    		<div class="col-lg-6">
+	    		<h1>Jakiej usługi szukasz?</h1>
+	    		<p>Staramy się abyś znalazł w naszym serwisie fachowca, który spełni Twoje oczekiwania.</p>
+	    	</div>
+	    	<div class="col-lg-6">
+	    		<img src="/img/janusz.png" />
+	    	</div>
+    	</div>
+
+    	<form id="wyszukiwarka" class="form-inline col-lg-12">
+    		<div class="col-lg-12 form-inline">
+	    		<input type="text" class="col-lg-6 form-control mb-2 mr-sm-0 mb-sm-0" id="szukajZlecen" placeholder="Jakiej usługi szukasz?">
+	    		<input type="text" class="col-lg-4 form-control mb-2 mr-sm-0 mb-sm-0" id="SzukajLokalizacji" placeholder="Miejscowość lub Województwo">
+	    		<button id="szukaj" class="btn btn-primary col-lg-2"><i class="fa fa-search" aria-hidden="true"></i> Szukaj</button>
     		</div>
     	</form>
 		
