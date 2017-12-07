80a4237635c64756ef2f7677dabf7c6dff69dd20
Solmyr93 <sebastianlapinski93@gmail.com>
2017-12-07 15:48:49 +0100

    rep2

---
+++ database/migrations/2017_07_25_073654_create_jobs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('queue', 50);
+            $table->longText('payload');
+            $table->tinyInteger('attempts', 50)->unsigned();
+            $table->unsignedInteger('reserved_at', 50)->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+
+            $table->index(['queue', 'reserved_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
