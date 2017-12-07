b3b94d86bce6bb0791b7f8a8f63386cff10c5a67
lqasz <lucas.osuch@wp.pl>
2017-12-07 13:56:17 +0100

    Laraspace - zmiany w bazie danych v1.2

--- database/migrations/2017_07_25_073654_create_jobs_table.php
+++ database/migrations/2017_07_25_073654_create_jobs_table.php
@@ -15,10 +15,10 @@ class CreateJobsTable extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('queue');
+            $table->string('queue', 50);
             $table->longText('payload');
-            $table->tinyInteger('attempts')->unsigned();
-            $table->unsignedInteger('reserved_at')->nullable();
+            $table->tinyInteger('attempts', 50)->unsigned();
+            $table->unsignedInteger('reserved_at', 50)->nullable();
             $table->unsignedInteger('available_at');
             $table->unsignedInteger('created_at');
 
