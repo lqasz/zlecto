cdafc87b178426be3a76e9c8dca744ba7f4f8734
lqasz <lucas.osuch@wp.pl>
2017-12-26 20:58:01 +0100

    Wysyłanie maili potwierdzających rejestrację v1.3

--- resources/views/index/sessions/partials/regiser-link.blade.php
+++ resources/views/index/sessions/partials/regiser-link.blade.php
@@ -1,11 +1,17 @@
-@component('mail::message', ['firt_name' => $first_name, 'token' => $token])
+<!-- @component('mail::message', ['firt_name' => $first_name, 'token' => $token])
 
 {{$first_name}},
 
-@component('mail::button', ['url' => 'http://10.40.10.10/register/'.{{$token}}])
+@component('mail::button', ['url' => ])
 Zarejestruj
 @endcomponent
 
 Dziękujemy,<br>
 {{ config('app.name') }}
-@endcomponent
+@endcomponent -->
+
+<div>
+	{{$first_name}}
+
+	<a href="http://10.40.10.10/register/'.{{$token}}" >Zarejestruj się!</a>
+</div>
